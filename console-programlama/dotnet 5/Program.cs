be5d31bd97b42b3127b2f713bc8f619bf7e2b19d
Agyad Semseddin <agyad.se@gmail.com>
2022-02-23 14:32:35 +0300

    Added C# examples for 5 and 6 versions

---
+++ console-programlama/dotnet 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dotnet_5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your surname ");
+            string surename = Console.ReadLine();
+            Console.WriteLine("Hi " + name + " "+ surename);
+        }
+    }
+}
