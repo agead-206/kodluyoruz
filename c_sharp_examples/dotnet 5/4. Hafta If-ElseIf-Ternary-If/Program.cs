886ac2b7e598cf40b0e3c3d2857c678ff7e00a59
Agyad Semseddin <agyad.se@gmail.com>
2022-02-28 16:43:28 +0300

    4. Hafta If-ElseIf-Ternary-If eklendi

---
+++ c_sharp_examples/dotnet 5/4. Hafta If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._Hafta_If_ElseIf_Ternary_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time <11)
+                Console.WriteLine("Gunaydin");
+            else if (time <=18)
+                Console.WriteLine("Iyi gunler");
+            else 
+                Console.WriteLine("Iyi gecler");
+            
+
+            String  sonuc =time<= 6 &&time <= 11 ? "gunaydin" : time<=18 ? "iyi gunler" : "iyi geceler"; 
+                Console.WriteLine(sonuc);
+
+        }
+    }
+}
