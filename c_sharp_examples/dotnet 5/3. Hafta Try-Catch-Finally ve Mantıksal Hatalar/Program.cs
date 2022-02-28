b9c7d9543b754253c9d4e01373b89def475d6ab0
Agyad Semseddin <agyad.se@gmail.com>
2022-02-28 15:47:05 +0300

    3. Hafta Hata kontrolu eklendi

---
+++ c_sharp_examples/dotnet 5/3. Hafta Try-Catch-Finally ve Mantıksal Hatalar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _3._Hafta_Try_Catch_Finally_ve_Mantıksal_Hatalar
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+             try
+        {
+            Console.WriteLine("sayi giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmis oldugunuz sayi: " + sayi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message.ToString());           
+        }
+        finally {
+            Console.WriteLine("Islem tamamlandi.");
+        }
+
+        {
+             try
+        {
+            //int a = int.Parse(null);
+            // int a = int.Parse("test");
+            int a = int.Parse("-12345678910");
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Bos deger girdiniz");  
+            Console.WriteLine(ex);           
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Veri tipi uygun degil");  
+            Console.WriteLine(ex);           
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("cok kutcut veya cok buyuk bir deger girdiniz");  
+            Console.WriteLine(ex);           
+        }
+        finally {
+            Console.WriteLine("ikinci islem tamamlandi");  
+        }
+        }
+    }
+}
+}
